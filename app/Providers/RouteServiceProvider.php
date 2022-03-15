adcad1d2d534f3f903da28e45e8248b6713703a5
Bryan Gallo <bryangallo95@hotmail.com>
2022-03-14 19:29:07 -0500

    Perfil edit,corregido error ruta

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,7 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     // public const HOME = '/home';
-    public const HOME = '/odontologia';
+    public const HOME = '/odontologias';
 
     /**
      * The controller namespace for the application.
